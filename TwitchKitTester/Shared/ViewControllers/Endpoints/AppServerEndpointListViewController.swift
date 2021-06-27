565ac67f7ae18e2af3d805e2691a5be628d10caf
Tyler Prevost <ttprevost@gmail.com>
2021-06-27 12:59:29 -0700

    Add async example to app server endpoint list VC

--- TwitchKitTester/Shared/ViewControllers/Endpoints/AppServerEndpointListViewController.swift
+++ TwitchKitTester/Shared/ViewControllers/Endpoints/AppServerEndpointListViewController.swift
@@ -15,15 +15,30 @@ class AppServerEndpointListViewController: PlatformIndependentTableViewControlle
                 return
             }
             
-            apiSession = .init(authSession: authSession)
+            let apiSession = ServerAppAPISession(authSession: authSession)
+            self.apiSession = apiSession
             
-            apiSession?.perform(LegacyGetChatBadgesByChannelRequest(channelId: twitchChannelId)) { result in
-                switch result {
-                case .success((let badges, _)):
-                    print("Response:", badges)
-                    
-                case .failure(let error):
-                    print("Error:", error)
+            if #available(iOS 15, macOS 12, *) {
+                async {
+                    let request = LegacyGetChatBadgesByChannelRequest(channelId: twitchChannelId)
+                    let (badges, _) = try await apiSession.perform(request)
+                    print("Subscriber badge:", badges.subscriber?.image ?? "nil")
+                    print("Admin badge:", badges.admin.image ?? "nil")
+                    print("Broadcaster badge:", badges.broadcaster.image ?? "nil")
+                    print("Global Mod badge:", badges.globalMod.image ?? "nil")
+                    print("Mod badge:", badges.mod.image ?? "nil")
+                    print("Staff badge:", badges.staff.image ?? "nil")
+                    print("Turbo badge:", badges.turbo.image ?? "nil")
+                }
+            } else {
+                apiSession.perform(LegacyGetChatBadgesByChannelRequest(channelId: twitchChannelId)) { result in
+                    switch result {
+                    case .success((let badges, _)):
+                        print("Response:", badges)
+                        
+                    case .failure(let error):
+                        print("Error:", error)
+                    }
                 }
             }
         }
